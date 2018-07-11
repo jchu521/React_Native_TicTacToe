c218b8bedd96b5d6fb6c9f56abeaf91b9059bb69
Kevin Liu <kevinlyh2016@gmail.com>
2018-07-11 20:42:51 +1200

    Ota
    Add codepush.

--- android/app/src/main/java/com/tictactoe/MainApplication.java
+++ android/app/src/main/java/com/tictactoe/MainApplication.java
@@ -3,6 +3,8 @@ package com.tictactoe;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.microsoft.codepush.react.CodePush;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -16,6 +18,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -25,6 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
+            new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG) //Kevin, CodePush
             new RNI18nPackage(),
             new VectorIconsPackage()
       );
