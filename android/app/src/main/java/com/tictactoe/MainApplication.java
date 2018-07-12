2f9d2f26975ee8866094b785eb1427883cea1905
Kevin Liu <kevinlyh2016@gmail.com>
2018-07-12 14:04:15 +1200

    Package bugs

--- android/app/src/main/java/com/tictactoe/MainApplication.java
+++ android/app/src/main/java/com/tictactoe/MainApplication.java
@@ -31,10 +31,11 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
+	  //Kevin, CodePush
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNDeviceInfo(),
-            new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG) //Kevin, CodePush
+            new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG),
             new RNI18nPackage(),
             new VectorIconsPackage()
       );
