19e230aa389bf29187ed9a0b6df3cc5d1b842439
Kevin Liu <kevinlyh2016@gmail.com>
2018-07-24 15:39:26 +1200

    Nodejs version for Online Game.

--- src/components/actions/Actions.js
+++ src/components/actions/Actions.js
@@ -75,3 +75,67 @@ export const newGame = (data) => (dispatch) => {
         });
     }).catch(C.error);
 }
+
+//steps
+export const getStepsNode = (data) => (dispatch) => {
+  const { uuid } = data;
+  let url = 'https://www.breakself.tech/steps?uuid='+encodeURIComponent(uuid);
+  // console.log(url);
+  fetch(url, {
+      headers: C.headers,
+      credentials: C.credentials
+  }).then(C.parseResponse).then(steps => {
+        dispatch({
+            type: C.GET_STEPS_NODE,
+            steps
+        });
+    }).catch(C.error)
+}
+
+//Post /step
+export const addStepNode = (data) => (dispatch) => {
+    const { uuid, index, position, piece } = data;
+    const content = {
+      headers: C.headers,
+      method: 'POST',
+      credentials: C.credentials,
+      body: JSON.stringify({
+          'uuid': uuid,
+          'index': index,
+          'position': position,
+          'piece': piece
+      })
+    };
+
+    fetch('https://www.breakself.tech/addstep', content).then(C.parseResponse).then( data => {
+        dispatch({
+            type: C.ADD_STEP_NODE,
+            step: data,
+        });
+    }).catch(C.error);
+}
+//rounds
+//PUT /round
+export const putResult = (data) => (dispatch) => {
+    const { uuid, status, winner, reason, lastStep } = data;
+    let url = 'https://www.breakself.tech/round/';
+    // console.log(url);
+    const content = {
+        headers: C.headers,
+        method: 'POST',
+        credentials: C.credentials,
+        body: JSON.stringify({
+            'uuid': uuid,
+            'status': status,
+            'winner': winner,
+            'reason': reason,
+            'lastStep': lastStep,
+        })
+    };
+    fetch(url, content).then(C.parseResponse).then((res) => {
+        dispatch({
+            type: C.PUT_RESULT,
+            res
+        });
+    }).catch(C.error);
+}
--- src/components/reducers/stepsReducer.js
+++ src/components/reducers/stepsReducer.js
@@ -6,10 +6,12 @@ const steps = (state=[], action) => {
     switch (action.type){
         case C.GET_STEPS:
             return steps;
-
         case C.ADD_STEP:
             return false;
-
+        case C.GET_STEPS_NODE:
+            return steps;
+        case C.ADD_STEP_NODE:
+            return false;
         default:
             return false;
     }
